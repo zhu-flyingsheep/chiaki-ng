d1465f766ea683e48713d7d63637233faf58a580
学飞的羊 <326599322@qq.com>
2025-04-02 14:58:29 +0800

    测试打印日志

--- gui/src/main.cpp
+++ gui/src/main.cpp
@@ -186,9 +186,9 @@ int real_main(int argc, char *argv[])
 	if(args.length() == 0){
 		// 安装自定义日志处理函数
 		qInstallMessageHandler(myMessageHandler);
+		qDEBUG() << "这是日志测试信息";
 
 		// 测试日志
-		qDebug() << "T===========================================DEBUG";
 		return RunMain(app, use_alt_settings ? &alt_settings : &settings, exit_app_on_stream_exit);
 
 	}
