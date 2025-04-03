56bd818889158d2607a62a92a47cc2798df44c79
学飞的羊 <326599322@qq.com>
2025-04-03 16:54:02 +0800

    增加打印日志

--- lib/src/discoveryservice.c
+++ lib/src/discoveryservice.c
@@ -117,39 +117,42 @@ CHIAKI_EXPORT ChiakiErrorCode chiaki_discovery_service_init(ChiakiDiscoveryServi
 	CHIAKI_LOGE(service->log, "Size of sockaddr_storage: %zu bytes", sizeof(struct sockaddr_storage));
 	CHIAKI_LOGE(service->log, "Size of options->send_addr: %zu bytes\n", sizeof(options->send_addr));
 	CHIAKI_LOGE(service->log, "Size of options->broadcast_addrs: %zu bytes\n", sizeof(options->broadcast_addrs));
-
 	CHIAKI_LOGE(service->log, "options->hosts_max: %zu", service->options.hosts_max);
 	CHIAKI_LOGE(service->log, "options->host_drop_pings: %zu", service->options.host_drop_pings);
 	CHIAKI_LOGE(service->log, "options->ping_ms: %zu", service->options.ping_ms);
-
-
     CHIAKI_LOGE(service->log, "options->ping_initial_ms: %zu\n", (service->options.ping_initial_ms));
 
     CHIAKI_LOGE(service->log, "options->send_addr: %s\n", print_sockaddr_storage(service->options.send_addr));
     CHIAKI_LOGE(service->log, "options->send_addr_size: %zu\n", service->options.send_addr_size);
-
     CHIAKI_LOGE(service->log, "options->broadcast_addrs:\n");
     for (size_t i = 0; i < service->options.broadcast_num; i++) {
         CHIAKI_LOGE(service->log, "  [%zu]: %s\n", i, print_sockaddr_storage(&service->options.broadcast_addrs[i]));
     }
     CHIAKI_LOGE(service->log, "options->broadcast_num: %zu\n", service->options.broadcast_num);
-
     CHIAKI_LOGE(service->log, "options->send_host: %s\n", service->options.send_host ? service->options.send_host : "NULL");
     CHIAKI_LOGE(service->log, "options->cb: %p\n", (service->options.cb));
     CHIAKI_LOGE(service->log, "options->cb_user: %p\n", service->options.cb_user);
+
+
+
+	
 	err = chiaki_discovery_init(&service->discovery, log, ((struct sockaddr *)service->options.send_addr)->sa_family);
 	if(err != CHIAKI_ERR_SUCCESS)
 		goto error_send_addr;
+	CHIAKI_LOGE(service->log, "chiaki_discovery_init===");
 
 	err = chiaki_bool_pred_cond_init(&service->stop_cond);
 	if(err != CHIAKI_ERR_SUCCESS)
 		goto error_discovery;
+	CHIAKI_LOGE(service->log, "chiaki_bool_pred_cond_init===");
 
 	err = chiaki_thread_create(&service->thread, discovery_service_thread_func, service);
 	if(err != CHIAKI_ERR_SUCCESS)
 		goto error_stop_cond;
+    CHIAKI_LOGE(service->log, "chiaki_thread_create===");
 
 	chiaki_thread_set_name(&service->thread, "Chiaki Discovery Service");
+    CHIAKI_LOGE(service->log, "chiaki_thread_set_name===");
 
 	return CHIAKI_ERR_SUCCESS;
 error_stop_cond:
