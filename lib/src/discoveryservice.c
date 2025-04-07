cda9d15c926d10299a603b618cbdef0f3830bc0f
学飞的羊 <326599322@qq.com>
2025-04-07 20:23:31 +0800

    注释日志

--- lib/src/discoveryservice.c
+++ lib/src/discoveryservice.c
@@ -268,7 +268,7 @@ static void discovery_service_ping(ChiakiDiscoveryService *service)
 		service->options.send_host = NULL;
 	}
 
-	CHIAKI_LOGV(service->log, "Discovery Service sending ping");
+	// CHIAKI_LOGV(service->log, "Discovery Service sending ping");
 	ChiakiDiscoveryPacket packet = { 0 };
 	bool send_extra_broadcast = false;
 	packet.cmd = CHIAKI_DISCOVERY_CMD_SRCH;
@@ -305,7 +305,7 @@ static void discovery_service_ping(ChiakiDiscoveryService *service)
 				if (!inet_ntop(((struct sockaddr_in *)(&service->options.broadcast_addrs[i]))->sin_family, &(((struct sockaddr_in *)(&service->options.broadcast_addrs[i]))->sin_addr), addr_string, sizeof(addr_string)))
 					CHIAKI_LOGE(service->log, "Discovery Service error with inet_ntop");
 				else
-					CHIAKI_LOGV(service->log, "Discovery Service pinged %s", addr_string);
+					// CHIAKI_LOGV(service->log, "Discovery Service pinged %s", addr_string);
 			}
 		}
 	}
@@ -381,9 +381,9 @@ static void discovery_service_host_received(ChiakiDiscoveryHost *host, void *use
 		chiaki_mutex_unlock(&service->state_mutex);
 		return;
 	}
-	CHIAKI_LOGV(service->log, "Discovery Service Received host with id %s", host->host_id);
-	CHIAKI_LOGV(service->log, "Discovery Service Received host with host_name %s", host->host_name);
-	CHIAKI_LOGV(service->log, "Discovery Service Received host with host_addr %s", host->host_addr);
+	// CHIAKI_LOGV(service->log, "Discovery Service Received host with id %s", host->host_id);
+	// CHIAKI_LOGV(service->log, "Discovery Service Received host with host_name %s", host->host_name);
+	// CHIAKI_LOGV(service->log, "Discovery Service Received host with host_addr %s", host->host_addr);
 
 	bool change = false;
 
