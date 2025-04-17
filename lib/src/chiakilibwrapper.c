85dcd1d2cfc1ef3a3f08d34e0c83f6f944916983
学飞的羊 <326599322@qq.com>
2025-04-17 15:46:06 +0800

    修改参数

--- lib/src/chiakilibwrapper.c
+++ lib/src/chiakilibwrapper.c
@@ -626,8 +626,8 @@ CHIAKI_EXPORT void VideoProcessFrame(AVFrame *frame, enum AVPixelFormat pixforma
     // 每次临时创建 SwsContext，避免全局缓存
     struct SwsContext *sws_ctx = sws_getContext(
         frame->width, frame->height, pixformat,
-        frame->width, frame->height, AV_PIX_FMT_BGR24,
-        SWS_BILINEAR, NULL, NULL, NULL);
+        frame->width, frame->height, AV_PIX_FMT_BGR48,
+        SWS_BICUBIC, NULL, NULL, NULL);
     if (!sws_ctx)
     {
         return;
@@ -640,7 +640,7 @@ CHIAKI_EXPORT void VideoProcessFrame(AVFrame *frame, enum AVPixelFormat pixforma
         return;
     }
 
-    rgb_frame->format = AV_PIX_FMT_BGR24;
+    rgb_frame->format = AV_PIX_FMT_BGR48;
     rgb_frame->width = frame->width;
     rgb_frame->height = frame->height;
 
